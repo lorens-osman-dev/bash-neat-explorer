dc3f38563466eede1c7ae9815995d32b3b7c6418
lorens osman <lorens.osman.dev@gmail.com>
2023-12-18 08:05:55 +0300

    change the style + useful commands function

--- lorens_neat_browser.sh
+++ lorens_neat_browser.sh
@@ -1,30 +1,91 @@
 #TODO:
+bn(){
+    if [ -z $1  ]; then
+         echo choose file to run with bun !! 
+    else
+        c && bun run $1 
+    fi
+}
+cdl(){
+    if [ -z $1  ]; then
+        exa --tree --level=1 --group-directories-first --icons 
+    else
+        cd $1 && exa --tree --level=1 --group-directories-first --icons 
+    fi
+}
+#like cd() function but show hidden foldres and files
+cdla(){
+    if [ -z $1  ]; then
+        exa --tree --level=1 --group-directories-first --icons -a
+    else
+        cd $1 && exa --tree --level=1 --group-directories-first --icons -a
+    fi
+}
+re(){
+ 
+    echo reloded
+     exec bash
+     
+}
+c(){
+    clear
+}
 
 #--------|[ Explorer ]|--------#
 e(){
-    app="/mnt/c/Windows/explorer.exe"
+    
     if [ -z $1  ]; then
-        $app .
+        nautilus .
     else
-        $app $1
+        nautilus $1
     fi
 }
 
 #--------|[ n() function ]|--------#
 n(){
-    app="/mnt/c/Windows/notepad.exe"
-    $app $1
+    
+    gnome-text-editor -i  $1
 }
 
-#--------|[ neat_explorer ]|--------#
+#--------|[ Usful Commands ]|--------#
 #When you use the "cd" command without any additional information, it will automatically 
 #take you to your home directory. The purpose of the "cd()" function is to cancel this behavior.
-#Note cd() invoked in neat_explorer()
-cd() {
-    [[ $# -eq 0 ]] && return
-    builtin cd "$@"
+
+
+function usful_commands() {
+    commands_file='/home/lorens/Files/usefull_commands.txt'
+    header='CommandsList'
+    # --border-label=lorens
+    
+    fzf_cool="
+    --reverse  
+    --border=sharp 
+    --prompt=$header
+    --pointer=➜
+    --disabled
+    --no-info
+    --height 40% 
+    --no-separator
+    --color=dark,hl:red:regular,fg+:white:regular,hl+:red:regular:reverse,query:white:regular,info:#cb4b16,prompt:#dd4814:bold,pointer:#dd4814:bold
+    "
+    if [ "$1" = "file" ]; then
+    echo "Commands List file path is : $commands_file" && gnome-text-editor -i $commands_file
+    elif [ "$1" = "add" ]; then
+    echo "Commands List file path is : $commands_file" && gnome-text-editor -i $commands_file
+    elif [ -z $1 ];
+    then
+        # grep for delete empty lines , and the lines that start with #
+        #awk command to filter out lines that start with “*” or “#”
+        #The sed -e 's/^[ \t]*//' part of the command will remove any spaces or tabs at the beginning of each line
+        cat $commands_file |grep -v '^$' | grep -v '^#'|sed -e 's/^[ \t]*//'| fzf $fzf_cool |  awk '!/^[\*]/' 
+    fi
+  
 }
-# n() notepad 
+alias uc="usful_commands"
+
+
+
+#--------|[]|--------#
 
 function neat_explorer() {
     # --border-label=lorens
@@ -36,15 +97,16 @@ function neat_explorer() {
     --info=inline:$info_inline_seprator
     --separator=""
     --prompt=[
-    --pointer=→
+    --pointer=➜
     --color=fg:#839496,bg+:#242424,spinner:#719e07,hl+:#5fff87,disabled:#ce392c   
     --color=header:#586e75,info:#cb4b16,pointer:#5fff87   
     --color=marker:#719e07,fg+:#839496,prompt:#5fff87,hl:#719e07
     "
+  
     cd $1
     if [ -z $1 ];
     then
-        selection="$(exa --tree --level=1 --group-directories-first --icons -a | fzf --header=$(pwd) $fzf_cool)"
+        selection="$(exa --tree --level=1 --group-directories-first --icons -a| fzf --header=$(pwd) $fzf_cool)"
         if [[ "${#selection}" -gt 6 ]]; then
             new_selection=${selection:6}
             if [[ -d "$new_selection" ]]
@@ -52,7 +114,7 @@ function neat_explorer() {
                     cd "$new_selection" && neat_explorer
                 elif [[ -f "$new_selection" ]]
                 then
-                    e "$new_selection"
+                    n "$new_selection"
                 fi
         elif [[ "${#selection}" -eq 3 ]]; then
             new_selection=${selection:1}
@@ -64,6 +126,8 @@ function neat_explorer() {
   
 }
 alias cd="neat_explorer"
+
+
 #--------|[]|--------#
 
 #--------|[ neat_hisory ]|--------#
@@ -116,5 +180,6 @@ bind -x '"\C-h": neat_history_search'
 #--------|[oh-my-posh-theme]|--------#
 #eval "$(oh-my-posh init bash --config ~/oh-my-posh-theme/lorens.omp.json)"
 #--------|[StarShip]|--------#
-export STARSHIP_CONFIG=~/lorens_bash/new.toml
+
+export STARSHIP_CONFIG=~/.lorens_bash/lorens_starship.toml
 eval "$(starship init bash)"
